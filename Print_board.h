903f18b692139a301645402e3404db941d4a0a14
oksssllyy <mitya.tomilov.99@mail.ru>
2019-03-12 12:43:44 +0700

    Board output functions.

---
+++ Print_board.h
@@ -0,0 +1,22 @@
+#ifndef PRINT_BOARD_H
+#define PRINT_BOARD_H
+#include "board.h"
+
+void print_board(struct Board point)
+{
+    for (size_t i = 0; i < 8; i++)
+    {
+	printf("%ld ", i+1);
+	for (size_t j = 0; j < 8; j++)
+	{
+	    printf("[%c] ", point.board[i][j]);
+	}
+    printf("\n");
+    }
+
+    for (size_t i = 0; i < 8; i++)
+    printf("   %c", letter[i]);
+
+    printf("\n");
+}
+#endif //PRINT_BOARD_H
