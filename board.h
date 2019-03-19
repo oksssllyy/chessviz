b8e07b384d6e23d5c7ca7ee8b74c48901bc5c695
oksssllyy <mitya.tomilov.99@mail.ru>
2019-03-19 12:11:14 +0700

    Add file "board".

--- board.h
+++ board.h
@@ -1,14 +1,13 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-    struct Board
-    {
-	char board[8][8];
-    };
-    struct Board point;
+struct Board {
+  char board[8][8];
+};
+struct Board point;
 
-    char letter[8] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+char letter[8] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 
-    void mk_board();
+void mk_board();
 
 #endif //BOARD_H
