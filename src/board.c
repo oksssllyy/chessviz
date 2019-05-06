2046698d26ca5b53a0db385720f98d6bf3d4cddf
oksssllyy <mitya.tomilov.99@mail.ru>
2019-05-06 14:54:23 +0700

    Implementation function 'charTOint' in board.c.

--- src/board.c
+++ src/board.c
@@ -4,6 +4,7 @@
 
 char input[6];
 int x1, y1, x2, y2;
+extern struct Board point;
 
 void infor(){
   printf("      Шахматы\n\n\n");
@@ -27,14 +28,14 @@ void input_data(side) {
         break;
       }
       printf("Произошла ошибочка. 
-              Попробуйте ввести заново, уважаемый шамхматист :)")
+              Попробуйте ввести заново ход, уважаемый шамхматист :)")
     }
     if (side == 1){
       if (white_figure()) {
         break;
       } else {
         printf("Произошла ошибочка. 
-                Попробуйте ввести заново, уважаемый шамхматист :)");
+                Попробуйте ввести заново ход, уважаемый шамхматист :)");
       }
     }
     if (side == 2){
@@ -42,24 +43,41 @@ void input_data(side) {
         break;
       } else {
         printf("Произошла ошибочка. 
-                Попробуйте ввести заново, уважаемый шамхматист :)");
+                Попробуйте ввести заново ход, уважаемый шамхматист :)");
       }
     } 
   }
 }
 
 int charTOint(input) {
-
-  char input[6];
+  if ((input[2] != '-') && (input[2] != 'x')) {
+    printf("Слушай, ошибочка вышла. Попробуй заново ввести: ");
+    return 0;
+  }
+  if ((input[2] == 'x') && point.board[x2][y2] == ' '){
+    printf("Никого нетю, чтобы рубить.\n");
+    return 0;
+  }
+  if ((input[2] == '-') && point.board[x2][y2] != ' '){
+    printf("Стой, стой, пожилой. А не пора ли рубить это место, а?\n");
+    return 0;
+  }
 
   fgets(input,6,stdin);
+  
   int x1 = (int)input[0] - 'A';
   int y1 = (int)input[1] - '1';
   int x2 = (int)input[3] - 'A';
   int y2 = (int)input[4] - '1';
   
-
+  if ((x1 >= 0) && (x1 < 8) && (y1 >= 0) && (y1 < 8) && 
+      (x2 >= 0) && (x2 < 8) && (y2 >= 0) && (y2 < 8)) {
+    return 1;
+  }
+  return 0;
 }
+
+
 
 
 
