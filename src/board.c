bba2ed7b4f62682cda1f0a37cafce19a934d297b
oksssllyy <mitya.tomilov.99@mail.ru>
2019-05-06 22:35:19 +0700

    Implementation function white_figure in file board.c.

--- src/board.c
+++ src/board.c
@@ -24,6 +24,18 @@ void input_data(side) {
       if (input == 'infor') {
         infor();
       }
+      if ((input[2] != '-') && (input[2] != 'x')) {
+        printf("Слушай, ошибочка вышла. Попробуй заново ввести: ");
+        return 0;
+      }
+      if ((input[2] == 'x') && point.board[x2][y2] == ' ') {
+        printf("Никого нетю, чтобы рубить.\n");
+        return 0;
+      }
+      if ((input[2] == '-') && point.board[x2][y2] != ' ') {
+        printf("Стой, стой. Не занято ли там, а?\n");
+        return 0;
+      }
       if (charTOint(input)){
         break;
       }
@@ -50,20 +62,6 @@ void input_data(side) {
 }
 
 int charTOint(input) {
-  if ((input[2] != '-') && (input[2] != 'x')) {
-    printf("Слушай, ошибочка вышла. Попробуй заново ввести: ");
-    return 0;
-  }
-  if ((input[2] == 'x') && point.board[x2][y2] == ' '){
-    printf("Никого нетю, чтобы рубить.\n");
-    return 0;
-  }
-  if ((input[2] == '-') && point.board[x2][y2] != ' '){
-    printf("Стой, стой, пожилой. А не пора ли рубить это место, а?\n");
-    return 0;
-  }
-
-  fgets(input,6,stdin);
   
   int x1 = (int)input[0] - 'A';
   int y1 = (int)input[1] - '1';
@@ -73,10 +71,70 @@ int charTOint(input) {
   if ((x1 >= 0) && (x1 < 8) && (y1 >= 0) && (y1 < 8) && 
       (x2 >= 0) && (x2 < 8) && (y2 >= 0) && (y2 < 8)) {
     return 1;
+  } else {
+    printf("Дружочек, кажется вы вылезли за пределы доски, не?\n")
+    return 0;
   }
   return 0;
 }
 
+int white_figure() {
+  if ((point.board[x2][y2] > 'A') && (point.board[x2][y2] < 'S')) {
+    printf("Упс, ведь там свои, не?\n");
+    return 0;
+  }
+  switch (point.board[x1][y1]) {
+    case 'P':
+      if ((y1 == 1) && (point.board == ' ') && (y2 == 2 || y2 == 3) && (x1 == x2) && (input[2] == '-')) {
+        return 1; //первый ход пешки
+      } 
+      if ((y2 - y1 == 1) && (x1 == x2) && (point.board[x2][y2] = ' ') && (input[2] == '-')) {
+        return 1; 
+      }
+      if ((point.board[x2][y2] > 'a' && point.board[x2][y2] < 's') && 
+          (x2 - x1 == 1 || x1 - x2 == 1) && (y2 - y1 == 1) && (input[2] == 'x')) {
+        return 1;
+      }
+      break;
+    case 'R':
+      if ((x1 == x2) && (y1 > y2 || y1 < y2)) {
+        return 1;
+      }
+      if ((y1 == y2) && (x1 > x2 || x1 < x2)) {
+        return 1;
+      }
+      printf("Не, дружок, однако так так ладья не ходит :(.\n")
+      break;
+    case 'N':
+      if ((y1 - y2 == 1 || y2 - y1 == 1) && (x1 - x2 == 2 || x2 - x1 == 2)) {
+        return 1;
+      }
+      if ((x1 - x2 == 1 || x2 - x1 == 1) && (y1 - y2 == 2 || y2 - y1 == 2)) {
+        return 1;
+      }
+    case 'B':
+      //чернополый слон
+      if (((x1 % 2 == 1) && (y1 % 2 == 1)) || ((x1 % 2 == 0) && (y1 % 2 == 0))) {
+        if (((x1 == 0 && y1 == 0) || (x1 == 7 && y1 == 7)) && (x2 == y2)) {
+          return 1;
+        } //по главной диагонали, при условии, что ход начинается из угла
+        if ((x2 + y2) == (x1 + y1) || (((x2 - x1) % 2 == 0) && ((y2 - y1) % 2 == 0)) || 
+            (((x2 - x1) % 2 == 1) && ((y2 - y1) % 2 == 1))) {
+          return 1;
+        } //остальные ходы
+      }
+      //белополый слон
+      if (((x1 % 2 == 0) && (y1 % 2 == 1)) || ((x1 % 2 == 1) && (y1 % 2 == 0))) {
+        if (((x1 == 0 && y1 == 7) || (x1 == 7 && y1 == )) && (x2 == y2))
+      }
+
+  }
+}
+
+int black_figure() {
+
+
+}
 
 
 
