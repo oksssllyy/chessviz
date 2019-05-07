12bc0b1cb86d43a95a0a15042dd273b4f26ba6ac
oksssllyy <mitya.tomilov.99@mail.ru>
2019-05-07 12:30:24 +0700

    Delete function make_board (board_print_plain.c)

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -1,95 +1,21 @@
 #ifndef BOARD_PRINT_PLAIN_C
 #define BOARD_PRINT_PLAIN_C
 #include "board_print_plain.h"
-
-void make_board() {
-
-  int i, j;
-
-  for (i = 0; i < 8; i++) {
-    char cell = ' ';
-    switch (i) {
-      case 0:
-      case 7:
-        cell = 'R';
-        break;
-      case 1:
-      case 6:
-        cell = 'N';
-        break;
-      case 2:
-      case 5:
-        cell = 'B';
-        break;
-      case 3:
-        cell = 'Q';
-        break;
-      case 4:
-        cell = 'K';
-        break;
-      default:
-        break;
-    }
-    point.board[0][i] = cell;
-  }
-
-  for (i = 0; i < 8; i++) {
-    point.board[1][i] = 'P';
-  }
-
-  for (i = 0; i < 8; i++) {
-    for (j = 2; j < 6; j++) {
-      point.board[j][i] = ' ';
-    }
-  }
-
-  for (i = 0; i < 8; i++) {
-    char cell = ' ';
-    switch (i) {
-      case 0:
-      case 7:
-        cell = 'r';
-        break;
-      case 1:
-      case 6:
-        cell = 'n';
-        break;
-      case 2:
-      case 5:
-        cell = 'b';
-        break;
-      case 3:
-        cell = 'q';
-        break;
-      case 4:
-        cell = 'k';
-        break;
-      default:
-        break;
-    }
-    point.board[7][i] = cell;
-  }
-
-  for (i = 0; i < 8; i++) {
-    point.board[6][i] = 'p';
-  }
-
-  print_board(point);
-}
+#include <stdio.h>
 
 void print_board() {
 
-  make_board();
+  char letter[8] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 
-  for (size_t i = 0; i < 8; i++) {
-    printf("%ld ", i + 1);
-    for (size_t j = 0; j < 8; j++) {
+  for (int i = 0; i < 8; i++) {
+    printf("%d ", i + 1);
+    for (int j = 0; j < 8; j++) {
       printf("[%c] ", point.board[i][j]);
     }
     printf("\n");
   }
 
-  for (size_t i = 0; i < 8; i++) {
+  for (int i = 0; i < 8; i++) {
     printf("   %c", letter[i]);
   }
 
