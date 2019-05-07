8d930d721c8737a23af85a3737ab8b1d89b94a22
oksssllyy <mitya.tomilov.99@mail.ru>
2019-05-07 12:29:21 +0700

    Delete function make_board (board_print_plain.h)

--- src/board_print_plain.h
+++ src/board_print_plain.h
@@ -1,6 +1,5 @@
 #ifndef BOARD_PRINT_PLAIN_H
 #define BOARD_PRINT_PLAIN_H
-#include "board.h"
 
 struct Board {
   char board[8][8];
@@ -8,10 +7,6 @@ struct Board {
 
 struct Board point;
 
-char letter[8] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
-
 void print_board();
-
-void make_board();
 
 #endif //BOARD_PRINT_PLAIN_H
